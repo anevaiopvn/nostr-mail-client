96e4a5b9fd2eb68eacede193e3321b2ab6dd73dc
Nogringo <npub1kg4sdvz3l4fr99n2jdz2vdxe2mpacva87hkdetv76ywacsfq5leqquw5te@uid.ovh>
2026-05-15 13:29:45 +0200

    feat(config): add nmail.li relay and Blossom server to recommended lists

--- lib/app/config/nostr_config.dart
+++ lib/app/config/nostr_config.dart
@@ -5,6 +5,7 @@ class NostrConfig {
     // 'wss://relay.nostr.band',
     'wss://relay.primal.net',
     'wss://relay.coinos.io',
+    'wss://relay.nmail.li',
     'wss://nostr-01.uid.ovh',
     'wss://nostr-02.uid.ovh',
     'wss://nostr-01.yakihonne.com',
@@ -13,6 +14,7 @@ class NostrConfig {
   static const recommendedInboxOutboxRelays = [
     'wss://relay.damus.io',
     'wss://relay.camelus.app',
+    'wss://relay.nmail.li',
     'wss://nostr-01.uid.ovh',
     'wss://nostr-02.uid.ovh',
     'wss://relay.primal.net',
@@ -20,12 +22,14 @@ class NostrConfig {
 
   static const recommendedDmRelays = [
     'wss://auth.nostr1.com',
+    'wss://relay.nmail.li',
     'wss://nostr-01.uid.ovh',
     'wss://nostr-02.uid.ovh',
   ];
 
   static const recommendedBlossomServers = [
     'https://blossom.yakihonne.com',
+    'https://blossom.nmail.li',
     'https://blossom-01.uid.ovh',
     'https://blossom-02.uid.ovh',
     'https://blossom.primal.net',
